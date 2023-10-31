ee2c14773b70c0f8f3ac41859e08f01c8197e4a2
Travis Downs <travis.downs@gmail.com>
2023-10-31 12:42:19 -0300

    Wrap everything in a class
    So we can pass global context like "verbose" around easily.

--- clang-format-find.py
+++ clang-format-find.py
@@ -69,132 +69,135 @@ CASES = sum([
     sum(len(v) + 1 for v in INT_OPTS.values()),
 ])
 
+class ClangFormat:
 
-def dump_config(style):
+    def dump_config(self, style: str):
 
-    args = ['clang-format', '--dump-config', f'--style={style}']
-    proc = subprocess.Popen(args, stdout=subprocess.PIPE)
-    ret = proc.wait()
-    if ret != 0:
-        print('clang-format call failed (output above)')
-        sys.exit(1)
+        args = ['clang-format', '--dump-config', f'--style={style}']
+        proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+        ret = proc.wait()
+        if ret != 0:
+            print('clang-format call failed (output above)')
+            sys.exit(1)
 
-    opts = {}
-    assert proc.stdout
-    for ln in proc.stdout.read().splitlines():
-        if ln and ln[0].isalpha() and ln[0].isupper():
-            key, val = ln.strip().split(':', 1)
-            opts[key] = val.strip()
+        opts = {}
+        assert proc.stdout
+        for ln in proc.stdout.read().splitlines():
+            if ln and ln[0].isalpha() and ln[0].isupper():
+                key, val = ln.strip().split(':', 1)
+                opts[key] = val.strip()
 
-    return opts
-
-
-def run(fn, opts, verbose=None):
-
-    style = ',\n'.join('%s: %s' % (k, v) for (k, v) in sorted(opts.items()))
-    args = ['clang-format', '-style={%s}' % style, fn]
-    if verbose:
-        print('\n\n', ' '.join(args), file=sys.stderr)
-
-    proc = subprocess.Popen(args, stdout=subprocess.PIPE)
-    ret = proc.wait()
-    if ret != 0:
-        raise RuntimeError(f'Failed to run clang-format command:\n' +
-                           ' '.join([f"'{a}'" for a in args]))
-    return proc.stdout.read().decode()
+        return opts
 
 
-def filescore(filename: str, opts: ConfigType, verbose: bool):
+    def run(self, fn, opts):
 
-    with open(filename) as f:
-        old = f.read()
+        style = ',\n'.join('%s: %s' % (k, v) for (k, v) in sorted(opts.items()))
+        args = ['clang-format', '-style={%s}' % style, fn]
+        if self.verbose:
+            print('\n\n', ' '.join(args), file=sys.stderr)
 
-    new = run(filename, opts, verbose)
-
-    res = 0
-    for ln in difflib.unified_diff(old.splitlines(), new.splitlines()):
-
-        if ln.startswith('---') or ln.startswith('+++') or ln.startswith('@'):
-            continue
-
-        if ln[0] in {'-', '+'}:
-            res += 1
-
-    if verbose:
-        print('Score: ', res)
-    return res
+        proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+        ret = proc.wait()
+        if ret != 0:
+            raise RuntimeError(f'Failed to run clang-format command:\n' +
+                            ' '.join([f"'{a}'" for a in args]))
+        return proc.stdout.read().decode()
 
 
-def score(files: list[str], opts: ConfigType, verbose: bool = False):
-    res = 0
-    for fn in files:
-        res += filescore(fn, opts, verbose)
-    return res
+    def filescore(self, filename: str, opts: ConfigType):
+
+        with open(filename) as f:
+            old = f.read()
+
+        new = self.run(filename, opts)
+
+        res = 0
+        for ln in difflib.unified_diff(old.splitlines(), new.splitlines()):
+
+            if ln.startswith('---') or ln.startswith('+++') or ln.startswith('@'):
+                continue
+
+            if ln[0] in {'-', '+'}:
+                res += 1
+
+        if self.verbose:
+            print('Score: ', res)
+
+        return res
 
 
-def show_progress(rel: float):
-    done = ('=' * int(round(70 * rel)))[:-1] + '>'
-    left = ' ' * int(round(70 * (1 - rel)))
-    sys.stderr.write('\r')
-    sys.stderr.write('[%s%s] %3s%%' % (done, left, int(round(rel * 100))))
-    sys.stderr.flush()
+    def score(self, files: list[str], opts: ConfigType):
+        res = 0
+        for fn in files:
+            res += self.filescore(fn, opts)
+        return res
 
 
-def main(args: list[str]):
+    def show_progress(self, rel: float):
+        done = ('=' * int(round(70 * rel)))[:-1] + '>'
+        left = ' ' * int(round(70 * (1 - rel)))
+        sys.stderr.write('\r')
+        sys.stderr.write('[%s%s] %3s%%' % (done, left, int(round(rel * 100))))
+        sys.stderr.flush()
 
-    verbose = False
-    if '-v' in args:
-        args.remove('-v')
-        verbose = True
 
-    if not args:
-        print('no files passed')
-        return
+    def main(self, args: list[str]):
 
-    for based_on in ALL_STYLES:
-        idx = 0
-        # best = dump_config(initial_style)
-        best: ConfigType = {'BasedOnStyle': based_on}
-        base = score(args, best)
-        if verbose:
-            print('Base score: ', base)
-        allopts = ALL_OPTS
-        for opt in sorted(allopts):
+        if '-v' in args:
+            args.remove('-v')
+            self.verbose = True
+        else:
+            self.verbose = False
 
-            if opt in BOOL_OPTS:
-                values = None, 'true', 'false'
-            elif opt in ENUM_OPTS:
-                values = [None] + list(ENUM_OPTS[opt])
-            elif opt in INT_OPTS:
-                values = [None] + list(INT_OPTS[opt])
-            elif opt in BASED_ON_STYLE:
-                values = list(BASED_ON_STYLE[opt])
-            else:
-                raise RuntimeError(f'bad opt: {opt}')
+        if not args:
+            print('no files passed')
+            return
 
-            for val in values:
+        for based_on in ALL_STYLES:
+            idx = 0
+            # best = dump_config(initial_style)
+            best: ConfigType = {'BasedOnStyle': based_on}
+            base = self.score(args, best)
+            if self.verbose:
+                print('Base score: ', base)
+            allopts = ALL_OPTS
+            for opt in sorted(allopts):
 
-                idx += 1
-                if not verbose:
-                    show_progress(idx / float(CASES))
-
-                opts = copy.copy(best)
-                if val is not None:
-                    opts[opt] = val
+                if opt in BOOL_OPTS:
+                    values = None, 'true', 'false'
+                elif opt in ENUM_OPTS:
+                    values = [None] + list(ENUM_OPTS[opt])
+                elif opt in INT_OPTS:
+                    values = [None] + list(INT_OPTS[opt])
+                elif opt in BASED_ON_STYLE:
+                    values = list(BASED_ON_STYLE[opt])
                 else:
-                    opts.pop(opt, None)
+                    raise RuntimeError(f'bad opt: {opt}')
 
-                test = score(args, opts, verbose)
-                if test < base:
-                    best[opt] = val
-                    base = test
+                for val in values:
 
-        print('', file=sys.stderr)
-        print(f'# Best configuration found based on: {based_on}')
-        print('# Score: %d' % base)
-        for k, v in sorted(best.items()):
-            print('%s: %s' % (k, v))
+                    idx += 1
+                    if not self.verbose:
+                        self.show_progress(idx / float(CASES))
+
+                    opts = copy.copy(best)
+                    if val is not None:
+                        opts[opt] = val
+                    else:
+                        opts.pop(opt, None)
+
+                    test = self.score(args, opts)
+                    if test < base:
+                        best[opt] = val
+                        base = test
+
+            print('', file=sys.stderr)
+            print(f'# Best configuration found based on: {based_on}')
+            print('# Score: %d' % base)
+            for k, v in sorted(best.items()):
+                print('%s: %s' % (k, v))
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    ClangFormat().main(sys.argv[1:])
