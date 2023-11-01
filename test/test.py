5cdafb90d49e1d9bd7a6c740a5b18de3922d5230
Travis Downs <travis.downs@gmail.com>
2023-10-31 23:27:24 -0300

    Add test1, goldfile writing

--- test/test.py
+++ test/test.py
@@ -2,6 +2,7 @@
 
 # acceptance test for clang-format-find
 
+import argparse
 import difflib
 import subprocess
 from pathlib import Path
@@ -16,7 +17,42 @@ root_dir = test_dir.parent
 all_srcs = list(map(str, src_dir.glob('*.cpp')))
 
 
+def splitlist(s: str):
+    return s.split(',')
+
+
+class TestDef(NamedTuple):
+    args: list[str]
+    goldfile: str
+
+
+all_tests: list[TestDef] = [
+    TestDef(all_srcs, 'test0'),
+    TestDef([str(src_dir / 'perf-timer.cpp')], 'test1'),
+]
+
+
 class OneTest:
+
+    def __init__(self):
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--tests',
+                            help='Comma separated list of tests to run',
+                            type=splitlist)
+        parser.add_argument(
+            '--write-goldfiles',
+            help='''When a difference is found in the actual output compared to
+            the expected output, update the goldfile with the actual output instead of failing the test.''',
+            action='store_true')
+
+        args = parser.parse_args()
+
+        self.tests = [
+            t for t in all_tests if not args.tests or t.goldfile in args.tests
+        ]
+
+        self.write: bool = args.write_goldfiles
 
     def run_cff(self, args: list[str]):
         args = [str(root_dir / 'clang-format-find.py')] + args
@@ -30,29 +66,32 @@ class OneTest:
         return output
 
     def gold_test(self, args: list[str], test_name: str):
-        file_name = test_name + '.txt'
-        gold_out = (gold_dir / file_name).read_text().splitlines()
+        goldfile = gold_dir / (test_name + '.txt')
+        try:
+            gold_out = goldfile.read_text().splitlines()
+        except FileNotFoundError:
+            if not self.write:
+                raise
+            gold_out = ''  # allow writing of non-existent goldfiles to populate them initially
+
         actual_out = self.run_cff(args).splitlines()
 
         if actual_out != gold_out:
-            print(f'TEST FAILED: {test_name}')
-            diff = difflib.unified_diff(gold_out,
-                                        actual_out,
-                                        fromfile=f'gold/{test_name}',
-                                        tofile=f'actual/{test_name}')
-            print('\n'.join(list(diff)))
-            # print(actual_out)
+            if self.write:
+                print(f'TEST FAILED (goldflie will be updated): {test_name}')
+                goldfile.write_text('\n'.join(actual_out) + '\n')
+            else:
+                print(f'TEST FAILED: {test_name}')
+                diff = difflib.unified_diff(gold_out,
+                                            actual_out,
+                                            fromfile=f'gold/{test_name}',
+                                            tofile=f'actual/{test_name}')
+                print('\n'.join(list(diff)))
 
             return False
 
         print(f'TEST PASSED: {test_name}')
         return True
-
-    class TestDef(NamedTuple):
-        args: list[str]
-        goldfile: str
-
-    tests: list[TestDef] = [TestDef(all_srcs, 'test0')]
 
     def run_all(self):
         passed = 0
